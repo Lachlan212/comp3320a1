79b57a482c6d3fade7e01e3c15fdcf88051952af
BuildTools <unconfigured@null.spigotmc.org>
2023-04-27 21:51:17 +1000

    CompareTo Q1 Changes

--- A1/demo/src/test/java/com/example/CompareTo.java
+++ A1/demo/src/test/java/com/example/CompareTo.java
@@ -9,24 +9,46 @@ public class CompareTo {
     
 
     @Test
-    public void YLessThanX(){ //X<Y
+    public void YLessThanXTrue(){ //X<Y
         BigInteger X = new BigInteger("10", 10);
         BigInteger Y = BigInteger.ONE;
         assertEquals(1, X.compareTo(Y));
     }
 
     @Test
-    public void EqualTo(){ //X<Y
+    public void YLessThanXFalse(){ //X!<Y
+        BigInteger X = BigInteger.ONE;
+        BigInteger Y = new BigInteger("10", 10);
+        assertNotEquals(1, X.compareTo(Y));
+    }
+
+
+    @Test
+    public void EqualToTrue(){ //X==Y
         BigInteger X = BigInteger.ONE;
         BigInteger Y = BigInteger.ONE;
         assertEquals(0, X.compareTo(Y));
     }
 
     @Test
-    public void YGreaterThanX(){ //X<Y
+    public void EqualToFalse(){ //X!=Y
+        BigInteger X = new BigInteger("10", 10);
+        BigInteger Y = BigInteger.ONE;
+        assertNotEquals(0, X.compareTo(Y));
+    }
+
+    @Test
+    public void YGreaterThanXTrue(){ //X>Y
         BigInteger X = new BigInteger("10", 10);
         BigInteger Y = BigInteger.ONE;
         assertEquals(-1, Y.compareTo(X));
+    }
+
+    @Test
+    public void YGreaterThanXFalse(){ //X!>Y
+        BigInteger X = BigInteger.ONE;
+        BigInteger Y = new BigInteger("10", 10);
+        assertNotEquals(-1, Y.compareTo(X));
     }
 
 }
