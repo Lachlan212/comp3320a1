cc823742e22549658bbaab1262758d69825a9c22
BuildTools <unconfigured@null.spigotmc.org>
2023-04-28 16:27:02 +1000

    commentt2

--- A1/demo/src/test/java/Q1/SignumConstructorTest.java
+++ A1/demo/src/test/java/Q1/SignumConstructorTest.java
@@ -3,6 +3,7 @@ import java.math.BigInteger;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+//Created by Lachlan Higgins (c3374994), for SENG3320 Assignment 1
 public class SignumConstructorTest{
 
     //For all of these cases, a failed test will either result in a null object, rather than a BigInteger, or a NumberFormatException is thrown.
@@ -50,32 +51,32 @@ public class SignumConstructorTest{
         
     }
 
-    // @Test
-    // public void SignumUpperBoundLimit(){
-    //     try{
-    //         byte[] X = {1,2,3,4,5};
-    //         int signum = 2;
-    //         BigInteger testVal = new BigInteger(signum, X);
-    //         assertNotNull(testVal);
-    //     }
-    //     catch(NumberFormatException e){
-    //         assertFalse(true);
-    //     }
+    @Test
+    public void SignumUpperBoundLimit(){
+        try{
+            byte[] X = {1,2,3,4,5};
+            int signum = 2;
+            BigInteger testVal = new BigInteger(signum, X);
+            assertNotNull(testVal);
+        }
+        catch(NumberFormatException e){
+            assertFalse(true);
+        }
        
-    // }
+    }
 
-    // @Test
-    // public void Signum0InvalidMag(){
-    //     try{
-    //         byte[] X = {1,2,3,4,5};
-    //         int signum = 0;
-    //         BigInteger testVal = new BigInteger(signum, X);
-    //         assertNotNull(testVal);
-    //     }
-    //     catch(NumberFormatException e){
-    //         assertFalse(true);
-    //     }
-    // }
+    @Test
+    public void Signum0InvalidMag(){
+        try{
+            byte[] X = {1,2,3,4,5};
+            int signum = 0;
+            BigInteger testVal = new BigInteger(signum, X);
+            assertNotNull(testVal);
+        }
+        catch(NumberFormatException e){
+            assertFalse(true);
+        }
+    }
 
     @Test
     public void Signum0ValidMag(){
