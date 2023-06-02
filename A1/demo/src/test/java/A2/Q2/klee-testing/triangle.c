787a31a7d23ef5e275438e19c087c9dd58193ee3
Matthew Clarke <86269255+matthew-clarke10@users.noreply.github.com>
2023-06-02 13:29:25 +1000

    Create triangle.c

---
+++ A1/demo/src/test/java/A2/Q2/klee-testing/triangle.c
@@ -0,0 +1,26 @@
+void triangle (int a, int b, int c){
+if ((a+b>c)&&(a+c>b)&&(b+c>a)) {
+if (a==b || a==c || b==c) {
+if (a==c && a==b)
+printf("equilateral triangle .\n");
+else if (a==c||b==c)
+printf("isosceles triangle.\n");
+}
+else
+printf("triangle.\n");
+}
+else
+printf("non-triangle.\n");
+return;
+}
+
+int main() {
+int a;
+klee_make_symbolic(&a, sizeof(a), "a");
+int b;
+klee_make_symbolic(&b, sizeof(b), "b");
+int c;
+klee_make_symbolic(&c, sizeof(c), "c");
+triangle(a, b, c);
+return 0;
+}
